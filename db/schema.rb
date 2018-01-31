7b7a09fe7bc8821af845c2488f6aaecfde8cf701
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-01-31 03:33:43 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131032923) do
+ActiveRecord::Schema.define(version: 20180131033116) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tag_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
